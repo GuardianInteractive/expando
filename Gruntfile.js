a238523a74da5d4f6b63056015b30c1dbd89e6ec
andymason <andrew.mason@guardian.co.uk>
2014-04-11 14:58:19 +0100

    init commit

---
+++ Gruntfile.js
@@ -0,0 +1,124 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    config: grunt.file.readJSON('src/settings.json'),
+
+    connect: {
+      server: {
+        options: {
+          port: 9001,
+          keepalive: true,
+          base: 'build'
+        }
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: ['src/*'],
+        tasks: ['clean', 'copy', 'replace'],
+        options: {
+          spawn: false,
+        },
+      },
+    },
+
+    replace: {
+      dist: {
+        options: {
+          patterns: [
+            {
+              match: /foo/g,
+              replacement: 'bar'
+            }
+          ]
+        },
+        files: [
+          {expand: true, flatten: true, src: ['src/boot.js'], dest: 'build/'}
+        ]
+      }
+    },
+
+    // replace: {
+    //   dist: {
+    //     options: {
+    //       patterns: [
+    //         {
+    //           match: 'launchImage',
+    //           replacement: '<%= config.launchImage %>'
+    //         },
+    //         {
+    //           match: 'iframeURL',
+    //           replacement: '<%= config.iframeURL %>'
+    //         },
+    //         {
+    //           match: 'iframeURL',
+    //           replacement: '<%= config.iframeURL %>'
+    //         }
+    //       ]
+    //     },
+    //     files: [
+    //       {expand: true, flatten: true, src: ['src/boot.js'], dest: 'build/'}
+    //     ]
+    //   }
+    // },
+
+    s3: {
+      test: {
+        options: {
+            bucket: 'gdn-cdn',
+            access: 'public-read',
+            headers: {
+              'Cache-Control': 'max-age=60, public',
+            },
+            debug: true
+        },
+        upload: [
+          {
+            src: 'boot.js',
+            dest: '/nextgen/<%= config.S3NextGenPath %>',
+          }
+        ]
+      },
+      prod: {
+        options: {
+            bucket: 'gdn-cdn',
+            access: 'public-read',
+            headers: {
+              'Cache-Control': 'max-age=60, public',
+            }
+        },
+        upload: [
+          {
+            src: 'boot.js',
+            dest: '/nextgen/<%= config.S3NextGenPath %>',
+          }
+        ]
+      }
+    },
+
+    copy: {
+      main: {
+        src: 'src/index.html',
+        dest: 'build/',
+        expand: true,
+        flatten: true
+      }
+    },
+
+    clean: ['build']
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-replace');
+  grunt.loadNpmTasks('grunt-s3');
+
+  grunt.registerTask('default', ['clean', 'copy','replace', 'connect', 'watch']);
+  grunt.registerTask('deploy', ['connect']);
+  grunt.registerTask('test-deploy', ['s3:test']);
+
+};
---
+++ src/boot.js
@@ -0,0 +1,69 @@
+define([], function () {
+
+    var remoteURL = '@@iframeURL';
+    var launcherImgSrc = '@@launchImage';
+    var iframe;
+
+    function removeElements(elementName) {
+        var elms = document.querySelectorAll(elementName);
+        for (var i = 0; i < elms.length; i++) {
+            var parent = elms[i].parentNode;
+            parent.removeChild(elms[i]);
+        }
+    }
+
+    function nukePage() {
+        removeElements('style');
+        removeElements('link');
+        removeElements('script');
+
+        document.body.innerHTML = '';
+        document.body.style.height = '100%';
+        document.body.style.width = '100%';
+        document.body.style.overflow = 'hidden';
+        document.body.removeAttribute('class');
+        document.body.removeAttribute('id');
+    }
+
+    function buildIframe() {
+        var iframe;
+        iframe = document.createElement('iframe');
+        iframe.style.width = '100%';
+        iframe.height = '100%';
+        iframe.frameBorder = '0';
+        iframe.style.border = 'none';
+        iframe.style.position = 'absolute';
+        iframe.style.left = '0';
+        iframe.style.top = '0';
+        iframe.src = remoteURL;
+        return iframe;
+    }
+
+    function buildLaunchPad() {
+        var launcher = document.createElement('div');
+        launcher.style.width = '100%';
+        launcher.style.height = '500px';
+        launcher.style.backgroundImage = 'url(' + launcherImgSrc  + ')';
+        launcher.style.backgroundRepeat = 'no-repeat';
+        launcher.style.backgroundSize = 'cover';
+
+        launcher.innerHTML = launcherHTML;
+        return launcher;
+    }
+
+    function goGoExpando() {
+        nukePage();
+        iframe = buildIframe();
+        document.body.appendChild(iframe);
+    }
+
+    function boot(el) {
+        var launcher = buildLaunchPad();
+        launcher.addEventListener('click', goGoExpando, false);
+        el.appendChild(launcher);
+    }
+
+    return {
+        boot: boot
+    };
+});
